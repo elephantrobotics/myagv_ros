79ff669004249ad317d063fb43a16409ff422ab4
X-lanni <91007097+X-lanni@users.noreply.github.com>
2024-04-19 17:18:45 +0800

    Update myAGV.cpp

--- src/myagv_odometry/src/myAGV.cpp
+++ src/myagv_odometry/src/myAGV.cpp
@@ -9,7 +9,7 @@
 const unsigned char header[2] = { 0xfe, 0xfe };
 
 boost::asio::io_service iosev;
-boost::asio::serial_port sp(iosev, "/dev/ttyS0");
+boost::asio::serial_port sp(iosev, "/dev/ttyAMA2");
 
 boost::array<double, 36> odom_pose_covariance = {
     {1e-9, 0, 0, 0, 0, 0,
